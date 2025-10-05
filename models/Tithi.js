297555bb7af5eeec4c1822612675e6d30f137eb4
Suhani Ranka <suhanijainn006@gmail.com>
2025-10-05 14:13:20 +0530

    fixed tithi

--- models/Tithi.js
+++ models/Tithi.js
@@ -3,8 +3,7 @@ import mongoose from 'mongoose';
 const tithiSchema = new mongoose.Schema({
   tithi: { type: String, required: true },
   date: { type: String, required: true }, // keep String since your current DB uses string dates
-  description: { type: String },
-  city: { type: String }
+  description: { type: String }
 }, { collection: 'Tithis' }); // ✅ Correct case-sensitive collection name
 
 const Tithi = mongoose.model('Tithi', tithiSchema);
